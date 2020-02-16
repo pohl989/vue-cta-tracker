071a18cbc8aed28f4b60fd190e160ac923b2eaa0
pohl989 <pohl989@gmail.com>
2020-02-15 19:12:34 -0600

    add first component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheRoutes />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheRoutes from './components/TheRoutes.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheRoutes
   }
 }
 </script>
---
+++ src/components/TheRoutes.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Hello World Route Information</div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
